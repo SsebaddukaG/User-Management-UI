cbf6b407cfd091bf3fbcd9eb126a84df5fb9db14
SsebaddukaG <luckygilbert1993@gmail.com>
2020-03-26 01:02:32 -0500

    sign-up/login page design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Header, Footer} from './Components/Layouts'
+import LoginForm from './Components/Forms/LoginForm'
 
 function App() {
   return (
     <div >
-      <Header/>
-      <h1>Hello from react</h1>
-      <Footer/>
+       <Header/>
+      <LoginForm/>
     </div>
   );
 }
--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,9 +1,40 @@
-import React from 'react'
 
-const Header = ()=>{
-    return(
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Header = () => {
+    const classes = useStyles();
+    return (
         <div>
-            <h1>hello from Header</h1>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Abato Manchadise
+                    </Typography>
+                    <Button color="inherit">Signup</Button>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
